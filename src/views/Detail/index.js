fba60d7564669befd3cc3fbb249ebc9e39412544
Edward <koo22952@gmail.com>
2020-07-24 00:10:15 +0800

    增加SCSS 全域樣式

--- src/views/Detail/index.js
+++ src/views/Detail/index.js
@@ -2,12 +2,12 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import withDetail from '../../service/Detail'
 
-function Detail () {
-    return (
-        <div className="App">
-            <Link to="/home"> 1244443GGYY</Link>
-        </div>
-    )
+function Detail() {
+  return (
+    <div className="App">
+      <Link to="/home"> 1244443GGYY</Link>
+    </div>
+  )
 }
 
 export default withDetail(Detail)
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -2,34 +2,36 @@ import React, { useEffect } from 'react'
 import { bookingRoom } from '../../api'
 import { Link } from 'react-router-dom'
 import withHome from '../../service/Home'
+import './index.scss'
 
 const booking = bookingRoom
 
-function Home () {
-    useEffect(() => {}, [])
+function Home() {
+  useEffect(() => {}, [])
 
-    const fetchBookingRoom = async () => {
-        const postData = {
-            name: 'HELL',
-            tel: '0987654321',
-            date: ['2020-08-20', '2020-08-21']
-        }
-        const id =
-            '3Elqe8kfMxdZv5xFLV4OUeN6jhmxIvQSTyj4eTgIowfIRvF4rerA2Nuegzc2Rgwu'
-
-        try {
-            const res = await booking(id, postData)
-            console.log(res)
-        } catch (err) {
-            console.error(err)
-        }
+  const fetchBookingRoom = async () => {
+    const postData = {
+      name: 'HELL',
+      tel: '0987654321',
+      date: ['2020-08-20', '2020-08-21'],
     }
+    const id =
+      '3Elqe8kfMxdZv5xFLV4OUeN6jhmxIvQSTyj4eTgIowfIRvF4rerA2Nuegzc2Rgwu'
 
-    return (
-        <div className="App">
-            <Link to="/detail">Homeeeeeeeeeeeee</Link>
-        </div>
-    )
+    try {
+      const res = await booking(id, postData)
+      console.log(res)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  return (
+    <div className="AppHome">
+      455645645
+      <Link to="/detail">Homeeeeeeeeeeeee</Link>
+    </div>
+  )
 }
 
 export default withHome(Home)
