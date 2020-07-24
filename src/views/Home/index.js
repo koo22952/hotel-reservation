691e1b18dee80e863cff8178410de051f145ef8e
Edward <tgfc057@tgfc.tw>
2020-07-24 17:29:07 +0800

    homePage layout 布局

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -4,34 +4,20 @@ import { Link } from 'react-router-dom'
 import withHome from '../../service/Home'
 import './index.scss'
 
-const booking = bookingRoom
+function Home () {
+    useEffect(() => {}, [])
 
-function Home() {
-  useEffect(() => {}, [])
-
-  const fetchBookingRoom = async () => {
-    const postData = {
-      name: 'HELL',
-      tel: '0987654321',
-      date: ['2020-08-20', '2020-08-21'],
-    }
-    const id =
-      '3Elqe8kfMxdZv5xFLV4OUeN6jhmxIvQSTyj4eTgIowfIRvF4rerA2Nuegzc2Rgwu'
-
-    try {
-      const res = await booking(id, postData)
-      console.log(res)
-    } catch (err) {
-      console.error(err)
-    }
-  }
-
-  return (
-    <div className="AppHome">
-      455645645
-      <Link to="/detail">Homeeeeeeeeeeeee</Link>
-    </div>
-  )
+    return (
+        <div className="home">
+            <div className="home-wrapper">
+                <h1 className='home-logo'><Link to="/detail">WHITE INN</Link></h1>
+                <div className='home-container'>
+                    <div className='home-rooms'>12345</div>
+                    <div className='home-footer'>45465465</div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default withHome(Home)
